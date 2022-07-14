ba8209dcf5211961604484adba996ba19500e4d6
Sam Walton <109100666+sammwalton@users.noreply.github.com>
2022-07-14 09:23:04 -0500

    Add files via upload

---
+++ fizzBuzz.js
@@ -0,0 +1,17 @@
+// Write your solution below this line:
+
+for(let i = 1; i < 51; i++){
+
+  if(i % 15 === 0){
+    console.log("fizzBuzz");
+  } else if(i % 3 === 0){
+    console.log("fizz")
+  } else if(i % 5 === 0){
+    console.log("buzz")
+  } else {
+    console.log(i)
+  }
+
+ }
+
+ 
